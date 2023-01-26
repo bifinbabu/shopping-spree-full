c0dc6eb46cb1c0987452422c20f74c461a20dc49
bifinbabu <bifinbabu088@gmail.com>
2023-01-26 18:33:24 +0530

    connected to mongodb atlas

--- shopping-spree-api/index.js
+++ shopping-spree-api/index.js
@@ -1,1 +1,22 @@
-console.log('hello2')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected successfully"))
+  .catch((error) => console.log(error));
+
+if (process.env.PORT) {
+  port = process.env.port;
+} else {
+  port = 5000;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
