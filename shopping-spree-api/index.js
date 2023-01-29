9549244a36fd1b543cba9e3619585c7ae37bb698
bifinbabu <bifinbabu088@gmail.com>
2023-01-29 17:50:04 +0530

    (Feature)Routing Initial, models created,  mongodb database change

--- shopping-spree-api/index.js
+++ shopping-spree-api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -10,6 +12,11 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database connected successfully"))
   .catch((error) => console.log(error));
+
+app.use(express.json());
+ 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 if (process.env.PORT) {
   port = process.env.port;
