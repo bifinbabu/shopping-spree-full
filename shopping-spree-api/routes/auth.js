91b7fe66572eaba784281707acc72204d33d0f20
bifinbabu <bifinbabu088@gmail.com>
2023-02-02 21:58:16 +0530

    (Feature) Login api, jwt initial accesstoken created

--- shopping-spree-api/routes/auth.js
+++ shopping-spree-api/routes/auth.js
@@ -1,19 +1,60 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // Register a new user
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(req.body.password, "Secret Passphrase"),
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+// Login user
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json("Wrong credentials");
+    }
+    // !user && res.status(401).json("Wrong credentials");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET
+    );
+    // console.log(hashedPassword);
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    // console.log(originalPassword);
+    if (originalPassword !== req.body.password) {
+      return res.status(401).json("Wrong credentials password");
+    }
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({others, accessToken});
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Invalid Credentials", data: error });
+    next();
   }
 });
 
